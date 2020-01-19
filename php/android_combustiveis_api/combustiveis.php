6a0b033fb9739d6f2796eb096dbb0fdac4351a44
João Rafael <47887885+JRafa96@users.noreply.github.com>
2020-01-19 12:16:25 +0000

    update combustiveis api
    return em json

--- php/android_combustiveis_api/combustiveis.php
+++ php/android_combustiveis_api/combustiveis.php
@@ -3,19 +3,32 @@ $con = mysqli_connect("localhost", "root", "", "android_api");
 
 if (mysqli_connect_errno($con)) {
     echo "Falha ao conectar: " . mysqli_connect_error();
+    die();
 }
 
 if (isset($_GET['tipo'])) {
-
     $tipo = $_GET['tipo'];
-    $result = mysqli_query($con, "SELECT * FROM precoscombustivel where tipo= '$tipo'");
-    $data = mysqli_fetch_all($result);
-    echo json_encode($data);
+
+
+    $stmt = $con->prepare("SELECT * FROM precoscombustivel where tipo= '$tipo'");
+    $stmt->execute();
+    $stmt->bind_result($id, $postoId, $tipoC, $preco);
+
+    $precos = array();
+
+    while ($stmt->fetch()) {
+        $temp = array();
+        $temp['id'] = $id;
+        $temp['postoId'] = $postoId;
+        $temp['tipo'] = $tipoC;
+        array_push($precos, $temp);
+    }
+
+    echo json_encode($precos);
     mysqli_close($con);
 }
 
 if (isset($_GET['postoId'])) {
-
     $postoId = $_GET['postoId'];
     $result = mysqli_query($con, "SELECT * FROM postoscombustivel where id= $postoId");
     $data = mysqli_fetch_object($result);
